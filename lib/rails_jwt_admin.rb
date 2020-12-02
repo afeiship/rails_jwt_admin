9619d526f7c060aedbf4736a74abff8d069b580a
afeiship <1290657123@qq.com>
2020-12-02 15:37:41 +0800

    fix: vendors/token not include

--- lib/rails_jwt_admin.rb
+++ lib/rails_jwt_admin.rb
@@ -1,4 +1,5 @@
 require "rails_jwt_admin/engine"
+require "vendors/token.rb"
 
 module RailsJwtAdmin
   # Your code goes here...
--- lib/rails_jwt_admin/engine.rb
+++ lib/rails_jwt_admin/engine.rb
@@ -2,6 +2,5 @@ module RailsJwtAdmin
   class Engine < ::Rails::Engine
     isolate_namespace RailsJwtAdmin
     config.generators.api_only = true
-    config.autoload_paths += Dir["#{config.root}/lib/vendors"]
   end
 end
