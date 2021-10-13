0364df8da06bac289fe69054d6aa91bdf10a5bfc
afeiship <1290657123@qq.com>
2021-10-13 08:16:15 +0800

    fix: conflict username

--- app/models/rails_jwt_admin/user.rb
+++ app/models/rails_jwt_admin/user.rb
@@ -2,6 +2,8 @@ module RailsJwtAdmin
   class User < ApplicationRecord
     has_secure_password
 
+    validates_uniqueness_of :username, :email
+
     def token
       { token: Token.encode(user_id: self.id) }
     end
