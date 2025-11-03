646a528492deadfff8e65e9e25de37a033408449
aric.zheng <1290657123@qq.com>
2025-11-03 17:57:26 +0800

    feat: add unique index

--- db/migrate/20201202013006_create_rails_jwt_admin_users.rb
+++ db/migrate/20201202013006_create_rails_jwt_admin_users.rb
@@ -1,14 +1,12 @@
 class CreateRailsJwtAdminUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :rails_jwt_admin_users do |t|
-      t.string :username, null: false
-      t.string :email, null: false
+      t.string :username, null: false, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
       t.string :password_digest, null: false
 
       t.timestamps
     end
 
-    add_index :rails_jwt_admin_users, :username, unique: true
-    add_index :rails_jwt_admin_users, :email, unique: true
   end
 end
