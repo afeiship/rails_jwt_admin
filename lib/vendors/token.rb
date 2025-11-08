451ac23f138d9e5fe0d74eaa7b212d1324fe6482
aric.zheng <1290657123@qq.com>
2025-11-08 10:39:29 +0800

    fix: sec key err

--- lib/vendors/token.rb
+++ lib/vendors/token.rb
@@ -2,14 +2,12 @@ require "rails_jwt_admin/engine"
 
 module RailsJwtAdmin
   class Token
-    SECRET_KEY = Rails.application.secrets.jwt_secret
-
     def self.encode(payload)
-      JWT.encode(payload, SECRET_KEY, 'HS256')
+      JWT.encode(payload, Rails.application.secrets.jwt_secret, 'HS256')
     end
 
     def self.decode(token)
-      HashWithIndifferentAccess.new(JWT.decode(token, SECRET_KEY, 'HS256')[0])
+      HashWithIndifferentAccess.new(JWT.decode(token, Rails.application.secrets.jwt_secret, 'HS256')[0])
     rescue
       nil
     end
